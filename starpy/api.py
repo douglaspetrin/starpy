0982916606b332ae4a91f0d0627f838556005258
Douglas Petrin <editor@douglaspetrin.com>
2019-07-14 17:04:16 -0300

    Alteração de nomes de métodos e novos filtros adicionados

--- starpy/api.py
+++ starpy/api.py
@@ -45,9 +45,12 @@ class GetStars(StarPyMae):
         """
         return self.get_people(rid)
 
+    def _get_transport(self, transport, res=None):
+        return self._sget(transport, res)
+
     def get_vehicles(self, res=None):
         """ Recebe vehicles """
-        return self._sget(self.VEHICLES, res)
+        return self._get_transport(self.VEHICLES, res)
 
     def get_vehicles_by_id(self, rid):
         """ Recebe vehicles por id
@@ -57,7 +60,7 @@ class GetStars(StarPyMae):
 
     def get_starships(self, res=None):
         """ Recebe starships """
-        return self._sget(self.STAR_SHIPS, res)
+        return self._get_transport(self.STAR_SHIPS, res)
 
     def get_starships_by_id(self, rid):
         """
@@ -67,14 +70,18 @@ class GetStars(StarPyMae):
         """
         return self.get_starships(rid)
 
-    def _find_pilots_from_df(self, transport):
-        """ Versão do tipo Dataframe """
-
+    def _if_transport(self, transport):
+        """ Verifica qual tipo de transporte está sendo requisitado """
+        v = None
         if transport == self.VEHICLES:
             v = self.get_vehicles()
         elif transport == self.STAR_SHIPS:
             v = self.get_starships()
+        return v
 
+    def _find_pilots_from_df(self, transport):
+        """ Versão do tipo Dataframe """
+        v = self._if_transport(transport)
         page, li2 = 1, []
         while v['next'] is not None:
             v = v['results']
@@ -98,18 +105,29 @@ class GetStars(StarPyMae):
     def find_pilots_from_s(self):
         return self._find_pilots_from_df(self.STAR_SHIPS)
 
-    def _find_fastest_df(self, transport):
-        """ Cria DataFrame dos transportes mais rápidos """
+    def _find_fastest_transport_name_and_its_speed(self, transport):
         df = self._df_of_transport(transport)
-        return df[2].nlargest(3).to_dict()
+        f, kl = df[2].nlargest(3), []
+        for i in range(len(f)):
+            n_index = f.index[i]
+            n_transporte = df[1][n_index]
+            kl.append(n_transporte)
+        return kl, f.to_dict()
+
+    def _find_fastest_speed(self, transport):
+        """ Cria DataFrame dos transportes mais rápidos """
+        return self._find_fastest_transport_name_and_its_speed(transport)[1]
+
+    def _find_fastest_trans_name(self, transport):
+        return self._find_fastest_transport_name_and_its_speed(transport)[0]
 
     def find_fastest_v(self):
         """ Encontra veículos mais rápidos e que tenham pilotos """
-        return self._find_fastest_df(self.VEHICLES)
+        return self._find_fastest_speed(self.VEHICLES)
 
     def find_fastest_s(self):
         """ Encontra naves mais rápidas e que tenham pilotos """
-        return self._find_fastest_df(self.STAR_SHIPS)
+        return self._find_fastest_speed(self.STAR_SHIPS)
 
     def _df_of_transport(self, transport):
         """ Cria DataFrame para fácil manipulação """
@@ -121,7 +139,7 @@ class GetStars(StarPyMae):
         return df
 
     def _url_of_fastest_pilot(self, transport):
-        fastest = self._find_fastest_df(transport)
+        fastest = self._find_fastest_speed(transport)
         fastest, url_list = list(fastest.keys()), []
         for i in range(len(fastest)):
             df, n = self._df_of_transport(transport), fastest[i]
@@ -161,9 +179,11 @@ class GetStars(StarPyMae):
         return self._names_pilots(self.STAR_SHIPS)
 
     def _name_and_max_speed(self, transport):
-        n, sp, lis = self._names_pilots(transport), list(self._find_fastest_df(transport).values()), []
+        n, sp, lis = self._names_pilots(transport), list(self._find_fastest_speed(transport).values()), []
+        trans = self._find_fastest_trans_name(transport)
+        transport_name = transport.capitalize()[:-1] + ' name'
         for i in range(len(n)):
-            li = [{n[i]: sp[i]}]
+            li = [{'Name': n[i], 'Max. Speed': sp[i], transport_name: trans[i]}]
             lis.append(li)
         return lis
 
@@ -173,17 +193,34 @@ class GetStars(StarPyMae):
     def name_and_max_speed_s(self):
         return self._name_and_max_speed(self.STAR_SHIPS)
 
+    def _by_idpeople_return_transport_speed(self, transport, idname):
+        lss, transport_urls_id = [], self.get_people_by_id(idname)[transport]
+        for i in range(len(transport_urls_id)):
+            url_id = transport_urls_id[i].split('/')[-2]
+            speed = self._by_idtransports_return_its_speed(transport, url_id)
+            ldata = [{'transport_id': url_id, 'Max. Speed': speed}]
+            lss.append(ldata)
+        return lss
 
+    def _by_idtransports_return_its_speed(self, transport, idtrans):
+        return self._get_transport(transport, idtrans)['max_atmosphering_speed']
 
+    def starships_speed_by_people(self, idpeople):
+        return self._by_idpeople_return_transport_speed(self.STAR_SHIPS, idpeople)
 
+    def vehicles_speed_by_people(self, idpeople):
+        return self._by_idpeople_return_transport_speed(self.VEHICLES, idpeople)
 
+    def _fastest_person(self, transport):
+        return self._name_and_max_speed(transport)[0]
 
+    def fastest_person_with_v(self):
+        """ Retorna o nome da pessoa, do vehicle e velocidade máxima """
+        return self._fastest_person(self.VEHICLES)
 
-
-
-
-
-
+    def fastest_person_with_s(self):
+        """ Retorna o nome da pessoa, da starship e velocidade máxima """
+        return self._fastest_person(self.STAR_SHIPS)
 
 
 s = GetStars()
